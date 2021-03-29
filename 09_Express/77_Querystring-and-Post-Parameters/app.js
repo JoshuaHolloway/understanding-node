30017c2f23b75e61118d9011c57ec259af6db495
JoshuaHolloway <jhollow6@asu.edu>
2021-03-29 14:22:17 -0500

    querystring

---
+++ 09_Express/77_Querystring-and-Post-Parameters/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+// Pass urlencodedParser as a middleware callback to .post method
+app.post('/person', urlencodedParser, function(req, res) {
+	res.send('Thank you!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+	res.send('Thank you for the JSON data!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
